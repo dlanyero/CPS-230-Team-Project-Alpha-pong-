dce3cfbfd4b6530a47f72662b58c2d8d9b606ace
Jeremiah-England <englandtuning@gmail.com>
2018-11-13 05:08:00 -0500

    printing gamestate

--- bju.asm
+++ bju.asm
@@ -11,10 +11,9 @@ main:
     mov     al, 0x1
     int     0x10                    ; set video to text mode
 
-    mov     word [es:bx+0], 0x9F42  ; B dark blue background, white font
-    mov     word [es:bx+2], 0x9F4A  ; J dark blue background, white font
-    mov     word [es:bx+4], 0x9F55  ; U dark blue background, white font
-    mov     word [es:bx+6], 0x1F21  ; ! dark blue background, white font
+    call print_score
+    call print_players
+    call print_ball
 
     mov     ah, 0x0                 ; wait for user input
     int     0x16
@@ -22,3 +21,63 @@ main:
     mov     ah, 0x4c                ; exit
     mov     al, 0
     int     0x21
+
+global print_score
+print_score:
+    mov     bx, 60                  ; you can assume that es has not been tampered with.
+                                    ; also assume that the video text mode has been set.
+    mov     word [es:bx+0], 0x0753  ; SCORE: --> {0x53, 0x43, 0x4f, 0x52, 0x45, 0x7c, space}
+    mov     word [es:bx+2], 0x0743  ; no blink. black background, non-bright white font.
+    mov     word [es:bx+4], 0x074f
+    mov     word [es:bx+6], 0x0752
+    mov     word [es:bx+8], 0x0745
+    mov     word [es:bx+10], 0x077c
+
+    mov     cx, 0x0730              ; the '0' character
+    add     cx, [score]
+    mov     word [es: bx + 14], cx
+    ret
+
+global print_players
+print_players:
+    mov     ax, 80                  ; multiplying to find the correct location to put the value.
+    mov     dx, word [player_paddle_loc]
+    imul    ax, dx
+    mov     bx, ax
+
+    mov     word [es:bx], 0x07db          ; 0xa6 --> block
+    mov     word [es:bx+80], 0x07db
+    mov     word [es:bx+160], 0x07db
+
+    mov     ax, 80                  ; multiplying to find the correct location to put the value.
+    mov     dx, word [comput_paddle_loc]
+    imul    ax, dx
+    mov     bx, ax
+
+    mov     word [es:bx+78], 0x07db          ; 0xdb --> block
+    mov     word [es:bx+158], 0x07db
+    mov     word [es:bx+238], 0x07db
+
+    ret
+
+global print_ball
+print_ball:
+    mov     ax, 80                  ; multiplying to find the correct location to put the value.
+    mov     dx, word [ball_y]
+    imul    ax, dx
+    mov     bx, ax                  ; y
+
+    add     bx, [ball_x]            ; x
+    mov     word [es: bx], 0x07db   ; print (x, y)
+    ret
+
+
+
+SECTION .data
+    player_paddle_loc: dw 12    ; [0, 22]
+    score: dw 2                 ; start score at 2 [0, 9]
+    ball_x: dw 40               ; [0, 78]
+    ball_y: dw 8                ; [0, 23]
+    ball_dx: dw 1               ; {-2, -1, 0, 1, 2}
+    ball_dy: dw 1               ; {-4, -2, 0, 2, 4}
+    comput_paddle_loc: dw 15    ; [0, 22]
--- pong.asm
+++ pong.asm
@@ -127,9 +127,79 @@ putstring:
 ; run through all the variables and print them out.
 ; how hard could that be?
 ; I just need to update the screen memory, since there is already a thread which
-; prints out the screen.
+; prints out the screen. Our screen is 80x25.
 print_screen:
     ; first I print the score in the top corner.
+    mov     ax, 0xB800
+    mov     es, ax                  ; moving directly into a segment register is not allowed
+    mov     bx, 80                 ; offset for approximately the middle of the screen
+
+    mov     ah, 0x0
+    mov     al, 0x1
+    int     0x10                    ; set video to text mode
+
+    call    print_score
+    call    print_players
+    call    print_ball
+
+    mov     ah, 0x0                 ; wait for user input
+    int     0x16
+
+    mov     ah, 0x4c                ; exit
+    mov     al, 0
+    int     0x21
+
+global print_score
+print_score:
+    mov     bx, 60                  ; you can assume that es has not been tampered with.
+                                    ; also assume that the video text mode has been set.
+    mov     word [es:bx+0], 0x0753  ; SCORE: --> {0x53, 0x43, 0x4f, 0x52, 0x45, 0x7c, space}
+    mov     word [es:bx+2], 0x0743  ; no blink. black background, non-bright white font.
+    mov     word [es:bx+4], 0x074f
+    mov     word [es:bx+6], 0x0752
+    mov     word [es:bx+8], 0x0745
+    mov     word [es:bx+10], 0x077c
+
+    mov     cx, 0x0730              ; the '0' character
+    add     cx, [score]
+    mov     word [es: bx + 14], cx
+    ret
+
+global print_players
+print_players:
+    mov     ax, 80                  ; multiplying to find the correct location to put the value.
+    mov     dx, word [player_paddle_loc]
+    imul    ax, dx
+    mov     bx, ax
+
+    mov     word [es:bx], 0x07db          ; 0xa6 --> block
+    mov     word [es:bx+80], 0x07db
+    mov     word [es:bx+160], 0x07db
+
+    mov     ax, 80                  ; multiplying to find the correct location to put the value.
+    mov     dx, word [comput_paddle_loc]
+    imul    ax, dx
+    mov     bx, ax
+
+    mov     word [es:bx+78], 0x07db          ; 0xdb --> block
+    mov     word [es:bx+158], 0x07db
+    mov     word [es:bx+238], 0x07db
+
+    ret
+
+global print_ball
+print_ball:
+    mov     ax, 80                  ; multiplying to find the correct location to put the value.
+    mov     dx, word [ball_y]
+    imul    ax, dx
+    mov     bx, ax                  ; y
+
+    add     bx, [ball_x]            ; x
+    mov     word [es: bx], 0x07db   ; print (x, y)
+    ret
+
+
+
 
 
     ; function body goes here
@@ -147,18 +217,18 @@ print_screen:
 ; . . . this is the hard function.
 
 SECTION .data
-    player_paddle_loc: dw 128   ; 256 possible values.
-    score: dw 100               ; start score at 100
-    ball_x: dw 128
-    ball_y: dw 128
+    player_paddle_loc: dw 12    ; [0, 22]
+    score: dw 2                 ; start score at 2 [0, 9]
+    ball_x: dw 40               ; even element of [0, 78]
+    ball_y: dw 8                ; [0, 23]
     ball_dx: dw 1               ; {-2, -1, 0, 1, 2}
-    ball_dy: dw 1               ; {-2, -1, 0, 1, 2}
-    comput_paddle_loc: dw 128   ;
+    ball_dy: dw 1               ; {-4, -2, 0, 2, 4}
+    comput_paddle_loc: dw 15    ; [0, 22]
 
-    current_task: dw 0 ; must always be a multiple of 2
+    current_task: dw 0          ; must always be a multiple of 2
     stacks: times (256 * 31) db 0 ; 31 fake stacks of size 256 bytes
-    task_status: times 32 dw 0 ; 0 means inactive, 1 means active
-    stack_pointers: dw 0 ; the first pointer needs to be to the real stack !
+    task_status: times 32 dw 0  ; 0 means inactive, 1 means active
+    stack_pointers: dw 0        ; the first pointer needs to be to the real stack !
                     dw stacks + (256 * 1)
                     dw stacks + (256 * 2)
                     dw stacks + (256 * 3)
